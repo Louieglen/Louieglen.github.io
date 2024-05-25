7acad58fad21e2a02a6351f80ce92ba86ec11d84
Louieglen <louieglencancino4@gmail.com>
2024-05-25 14:45:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const btnLikes1 = document.getElementById(btnLikes1")
+const btnLikes1 = document.getElementById("btnLikes1")
 const countLikes1 = documemnt.getElementById("countsLikes1")
+
+function clickLike1(){
+    let totalLikes1 = parseInt(countLikes1.values) + 1
+    countlikes1.textContent = totalLikes1.toString()
+}
