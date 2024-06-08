caf28a1b72cf0e870abc3b964944b30196e12195
Louieglen <louieglencancino4@gmail.com>
2024-06-08 15:33:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function clickLike1() {
   countLikes1.textContent = totalLikes1.toString()
 }
 function clickDislike1() {
-  let totalLikes1 = parseInt(countDislikes1.value) + 1
+  let totalDislikes1 = parseInt(countDislikes1.value) + 1
   countDislikes1.textContent = totalDislikes1.toString()
 }
 function clickLike2() {
@@ -19,13 +19,14 @@ function clickLike2() {
   countLikes2.textContent = totalLikes2.toString()
 }
 function clickDislike2() {
-  let totalLikes2 = parseInt(countDislikes2.value) + 1
+  let totalDislikes2 = parseInt(countDislikes2.value) + 1
   countDislikes2.textContent = totalDislikes2.toString()
 }
 btnLike1.addEventListener("click",clickLike1)
 btnLike2.addEventListener("click",clickLike2)
 btnDislike1.addEventListener("click",clickDislike1)
 btnDislike2.addEventListener("click",clickDislike2)
+
 const submit = document.getElementById("submit")
 const comment = document.getElementById("comment")
 const commentbox = document.getElementById("commentbox")
