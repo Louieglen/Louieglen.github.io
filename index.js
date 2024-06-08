41edd5b9cb8fb32664793450fe8622d87fdeaa4f
Louieglen <louieglencancino4@gmail.com>
2024-06-08 15:42:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const btnLike1 = document.getElementById("btnLike1")
-const btnDislike1 = document.getElementById("btnDislike1")
 const countLikes1 = document.getElementById("countLikes1")
+const btnDislike1 = document.getElementById("btnDislike1")
 const countDislikes1 = document.getElementById("countDislikes1")
 const btnLike2 = document.getElementById("btnLike2")
 const countLikes2 = document.getElementById("countLikes2")
@@ -10,13 +10,13 @@ function clickLike1() {
   let totalLikes1 = parseInt(countLikes1.value) + 1
   countLikes1.textContent = totalLikes1.toString()
 }
-function clickDislike1() {
-  let totalDislikes1 = parseInt(countDislikes1.value) + 1
-  countDislikes1.textContent = totalDislikes1.toString()
-}
 function clickLike2() {
   let totalLikes2 = parseInt(countLikes2.value) + 1
   countLikes2.textContent = totalLikes2.toString()
+}
+function clickDislike1() {
+  let totalDislikes1 = parseInt(countDislikes1.value) + 1
+  countDislikes1.textContent = totalDislikes1.toString()
 }
 function clickDislike2() {
   let totalDislikes2 = parseInt(countDislikes2.value) + 1
@@ -33,4 +33,4 @@ const commentbox = document.getElementById("commentbox")
 function submitComment(){
   commentbox.textContent += comment.value.toString() + "\n"
 }
-submit.addEventListener("click", submitComment)
+  submit.addEventListener("click", submitComment)
